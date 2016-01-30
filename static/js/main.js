fcedaafa32d1fd2267353fb7b1229b76001989b7
Jamie McArdle <mc4rdle@gmail.com>
2016-01-30 17:29:51 +0000

    Initial Commit

---
+++ static/js/main.js
@@ -0,0 +1,51 @@
+// Photo Grid
+
+$('.photoset-grid-lightbox').photosetGrid({
+  highresLinks: true,
+  rel: 'withhearts-gallery',
+
+  onComplete: function(){
+    $('.photoset-grid-lightbox').attr('style', '');
+    $('.photoset-grid-lightbox a').colorbox({
+      photo: true,
+      scalePhotos: true,
+      maxHeight:'70%',
+      maxWidth:'90%'
+    });
+  }
+});
+
+// Smooth Scroll Anchor
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+// Load Page
+
+$(document).ready(function() {
+    $("body").css("display", "none");
+ 
+    $("body").fadeIn(500);
+ 
+    $("a.link").click(function(event){
+        event.preventDefault();
+        linkLocation = this.href;
+        $("body").fadeOut(500, redirectPage);      
+    });
+         
+    function redirectPage() {
+        window.location = linkLocation;
+    }
+});
