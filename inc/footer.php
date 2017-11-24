2212bf8885eaa4b2638a595dc8cf94ddf6121162
Jamie McArdle <mc4rdle@gmail.com>
2017-11-24 07:22:03 +0000

    Change to HTML

--- inc/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="home">
-	<a href="studio.php">Studio</a>
-	<a href="news.php">News</a>
-	<a href="contact.php">Contact</a>
-</footer>
--- inc/header.php
+++
@@ -1,6 +0,0 @@
-<header class="home">
-	<nav>
-		<a href="http://ninety.co"><img class="logo" src="img/misc/logo.png" alt="Ninety Media Logo"></a>
-		<div class="menu-btn"><svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0" y="0" viewBox="0 0 24 18" xml:space="preserve" style="fill: #fff;"><rect width="24" height="2"></rect><rect y="8" width="24" height="2"></rect><rect y="16" width="24" height="2"></rect></svg></div>
-	</nav>
-</header>
